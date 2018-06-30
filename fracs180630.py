64de52c8e513a2534476c3e267e028e78797b65f
BARarch <anthonyquivers@gmail.com>
2018-06-30 13:34:15 -0400

    Python does big integers, really big ints

---
+++ fracs180630.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return n * extraLongFactorials(n - 1)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    print(extraLongFactorials(n))
