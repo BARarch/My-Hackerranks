178beb5afe6d810bf44a6ba892cf0db6e6c7d8d7
BARarch <anthonyquivers@gmail.com>
2018-07-22 12:37:11 -0400

    Set Work

---
+++ sets180722.py
@@ -0,0 +1,10 @@
+def average(array):
+    # your code goes here
+    arraySet = set(array)
+    
+    return sum(arraySet) / len(arraySet) 
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
