86d28773e4ae878f6b6930c2b3f0602e258e1190
BARarch <anthonyquivers@gmail.com>
2018-08-09 21:26:23 -0400

    clean set intersection example

---
+++ intersection180809.py
@@ -0,0 +1,6 @@
+a = int(input())
+engSet = set(list(map(int, input().split(' '))))
+b = int(input())
+frenchSet = set(list(map(int, input().split(' '))))
+
+print(len(engSet.intersection(frenchSet)))
