557e5553a021244f1d729c71f278fdb520152c7e
BARarch <anthonyquivers@gmail.com>
2018-08-22 18:03:55 -0400

    Timer decorators

--- xorIncr.py
+++ xorIncr.py
@@ -2,6 +2,8 @@
 
 ## XOR Incrementation or Exclusive OR Icremetation
 
+from datetime import *
+
 def bin_field(num, width=8):
 	return bin(num)[2:].rjust(width, '0')
 
@@ -14,7 +16,27 @@ def test_line(start, length):
 	for i in range(start, start + length):
 		xo ^= i
 		print('  ^{} = {}'.format(str(i).ljust(8), bin_field(xo)))
+	return xo
 
+
+def time_this(process):
+	def process_wrapper(*args, **kwargs):
+		startTime = datetime.now()
+		result = process(*args, **kwargs)
+		print(str(datetime.now() - startTime))
+		return result
+	return process_wrapper
+
+@time_this
+def line_xor(start, length):
+	littleXOR = 0
+	for i in range(start, start + length):
+		littleXOR ^= i
+
+	return littleXOR
+
+@time_this
+## Perfectly Accurate Definition for Problem
 def slow_xor(start, length):
 	bigXOR = 0
 	for i in range(length):
