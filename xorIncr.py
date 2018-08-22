37c3b7e7d64dcfea5f3d2913f2c72652c9d2dab7
BARarch <anthonyquivers@gmail.com>
2018-08-22 17:12:52 -0400

    Hey they kicked me out

--- xorIncr.py
+++ xorIncr.py
@@ -7,3 +7,22 @@ def bin_field(num, width=8):
 
 def last(lines, start=0):
 	return start + lines * (lines - 1)
+
+
+def test_line(start, length):
+	xo = 0
+	for i in range(start, start + length):
+		xo ^= i
+		print('  ^{} = {}'.format(str(i).ljust(8), bin_field(xo)))
+
+def slow_xor(start, length):
+	bigXOR = 0
+	for i in range(length):
+		littleXOR = 0
+		startt = start + (i * length)
+		#print(startt)
+		for j in range(startt, (startt + length - i)):
+			littleXOR ^= j
+		bigXOR ^= littleXOR
+
+	return bigXOR
