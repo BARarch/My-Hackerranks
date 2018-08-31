35eb81cda16bd3be43152172af83fc1809723572
BARarch <anthonyquivers@gmail.com>
2018-08-30 22:06:02 -0400

    A nice review

---
+++ leftRotate180830.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+    
+    fore = a[:d]
+    aft = a[d:]
+    print(' '.join(map(str, aft + fore)))
