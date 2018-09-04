ffb791cc02918b6d7531f3ee7f8652a40a1636c6
BARarch <anthonyquivers@gmail.com>
2018-09-03 21:08:21 -0400

    Array to Hash Demo

---
+++ sparse180903.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    stringHash = {}
+    for string in strings:
+        if string in stringHash:
+            stringHash[string] += 1
+        else:
+            stringHash[string] = 1
+    solns = []    
+    for query in queries:
+        if query in stringHash:
+            solns.append(stringHash[query])
+        else:
+            solns.append(0)
+            
+    return solns
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
