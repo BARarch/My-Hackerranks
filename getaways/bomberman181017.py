b3a03ebe53531d691bd41e221b5868296df128eb
BARarch <anthonyquivers@gmail.com>
2018-10-18 16:17:22 -0400

    Mathematical Asemtotic Solution and Utility Functions

--- getaways/bomberman181017.py
+++ getaways/bomberman181017.py
@@ -7,6 +7,43 @@ import re
 import sys
 
 # Complete the bomberMan function below.
+def encode(grid):
+    val = 0
+    for row in grid:
+        for c in row
+            val = val << 1
+            if c == 'O':
+                val += 1
+    return val
+
+def decode(val, r, c):
+    grid = []
+    col = 0
+    row = ''
+    for digit in bin(val)[2:]:
+        row += digit
+        col += 1
+        if col == c:
+            grid.append(row)
+            row = ''
+            col = 0
+    return grid
+
+def full_mask(r, c):
+    pass
+
+def left_mask(r, c):
+    pass
+
+def right_mask(r, c):
+    pass
+
+def make_explode(r, c):
+    ## Composes the explode function exclusivly for grids
+    ## with r rows and c columns
+    pass
+
+
 def left_right(row):
     return  row ^ ((row << 1) | 2 ^ len(row)) ^ (row >> 1)
 
