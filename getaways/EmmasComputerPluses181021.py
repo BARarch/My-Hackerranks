24f5e7a0a569f46e3185ca21e5b73ac803006b4a
BARarch <anthonyquivers@gmail.com>
2018-10-21 21:22:45 -0400

    Interesting Problem

---
+++ getaways/EmmasComputerPluses181021.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoPluses function below.
+def twoPluses(grid):
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    grid = []
+
+    for _ in range(n):
+        grid_item = input()
+        grid.append(grid_item)
+
+    result = twoPluses(grid)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
