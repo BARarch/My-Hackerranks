8313b20cd09dae4889b39d60013431ed751bf6ad
Anthony Quivers <anthonyquivers@gmail.com>
2018-10-27 21:42:11 -0400

    Divide by 10 and do an AND

---
+++ getaways/gridSearch181027.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the gridSearch function below.
+def gridSearch(G, P):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        RC = input().split()
+
+        R = int(RC[0])
+
+        C = int(RC[1])
+
+        G = []
+
+        for _ in range(R):
+            G_item = input()
+            G.append(G_item)
+
+        rc = input().split()
+
+        r = int(rc[0])
+
+        c = int(rc[1])
+
+        P = []
+
+        for _ in range(r):
+            P_item = input()
+            P.append(P_item)
+
+        result = gridSearch(G, P)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
