57bc813910d2baf0712d6e2d80034527954a2367
BARarch <anthonyquivers@gmail.com>
2018-11-02 17:53:58 -0400

    Time check at least n log n works

---
+++ getaways/almostSorted181102.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the almostSorted function below.
+def almostSorted(arr):
+    s = sorted(arr)
+    
+    return 'no'
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    almostSorted(arr)
