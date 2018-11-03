8bfaef88429ea43794dde2210d39f7c33ac3c99a
Anthony Quivers <anthonyquivers@gmail.com>
2018-11-03 12:01:43 -0400

    TestCase Reader

--- getaways/almostSorted181102.py
+++ getaways/almostSorted181102.py
@@ -6,34 +6,51 @@ import random
 import re
 import sys
 
+class TestCase:
+	def __init__(self, fileName):
+		f = open(fileName, 'r')
+		self.n = int(f.readline().rstrip())
+		self.arr = list(map(int, f.readline().rstrip().split(' ')))
+
+		f.close()
+
+	def get_n(self):
+		return self.n
+
+	def get_arr(self):
+		return self.arr
+
 def peak_val(arr):
+	peaks = []
+	vals = []
+	last = 0
+	n = 1
+	increasing = True
+	for c in arr:
+		#s = sorted(arr)
+		if increasing:
+			if c < last:
+				peaks.append(n - 1)
+				increasing = False
+		else:
+			if c > last:
+				vals.append(n - 1)
+				increasing = True
+		last = c
+		n += 1
+
+	return (peaks, vals)
 	
 
 # Complete the almostSorted function below.
 def almostSorted(arr):
-	peaks = []
-	vals = []
-	last = None
-	n = 1
-	increasing = True
-	for c in arr:
-	    #s = sorted(arr)
-	    if increasing:
-	    	if c < last:
-	    		peaks.append(n - 1)
-	    		increasing = False
-	    else:
-	    	if c > last:
-	    		vals.append(n - 1)
-	    		increasing = True
-    	last = c
-    	n += 1
+	peaks, vals = peak_val(arr)
 
-    return 'no'
+	return 'no'
 
 if __name__ == '__main__':
-    n = int(input())
+	n = int(input())
 
-    arr = list(map(int, input().rstrip().split()))
+	arr = list(map(int, input().rstrip().split()))
 
-    almostSorted(arr)
+	almostSorted(arr)
