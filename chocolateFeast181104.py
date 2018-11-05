6e083f9670890d74f37a666f4dbadbc4f7e7cfbb
Anthony Quivers <anthonyquivers@gmail.com>
2018-11-04 21:34:14 -0500

    and for the sixth star

---
+++ chocolateFeast181104.py
@@ -0,0 +1,39 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the chocolateFeast function below.
+def chocolateFeast(n, c, m):
+    chcs = n // c
+    wrappers = chcs
+    while wrappers >= m:
+        chcs2 = wrappers // m
+        chcs += chcs2
+        wrappers %= m
+        wrappers += chcs2
+    return chcs
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        ncm = input().split()
+
+        n = int(ncm[0])
+
+        c = int(ncm[1])
+
+        m = int(ncm[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
