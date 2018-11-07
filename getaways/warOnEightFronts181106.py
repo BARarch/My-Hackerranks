648650670f7c30539de5bc70a069169d3f45b88f
BARarch <anthonyquivers@gmail.com>
2018-11-06 23:49:17 -0500

    Structuring the unstructuralble

---
+++ getaways/warOnEightFronts181106.py
@@ -0,0 +1,11 @@
+### Structure the unstructurable
+### Believe in yourself
+
+## Front 1: Kiji
+## Front 2: Basement
+## Front 3: HomoAlgoriticus
+## Front 4: Grad Apps
+## Front 5: Google Recriutment
+## Front 6: Money Things
+## Front 7: Higher Ed Jobs
+## Front 8: ???
