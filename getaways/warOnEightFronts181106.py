3268db3470f7abf01b45510168ae475b789bc006
BARarch <anthonyquivers@gmail.com>
2018-11-07 00:07:14 -0500

    Structure Improvements

--- getaways/warOnEightFronts181106.py
+++ getaways/warOnEightFronts181106.py
@@ -1,11 +1,12 @@
 ### Structure the unstructurable
 ### Believe in yourself
 
-## Front 1: Kiji
-## Front 2: Basement
-## Front 3: HomoAlgoriticus
-## Front 4: Grad Apps
-## Front 5: Google Recriutment
-## Front 6: Money Things
-## Front 7: Higher Ed Jobs
-## Front 8: ???
+## Front 1: Kiji                 -- Large Scale Entrepreneur Sector
+## Front 2: Basement             -- Studio Development Sector
+## Front 3: HomoAlgoriticus      -- Architecture Content Marketing Sector
+## Front 4: Grad Apps            -- Research Sector
+## Front 5: Google Recriutment   -- Software Practices Sector
+## Front 6: Money Things         -- Solo Entrepreneur Sector
+## Front 7: Higher Ed Jobs       -- Curiousity Sector
+## Front 8: Stadium Architecture -- Archtitecture Sector
+
