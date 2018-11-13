b7a8969e2f23551c2501cc6b305602e821b7dabe
Anthony Quivers <anthonyquivers@gmail.com>
2018-11-13 14:24:28 -0500

    Allmost one shot

---
+++ arraySecondLargest181113.js
@@ -0,0 +1,47 @@
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    nums.sort(function (x,y) { return (x < y); });
+    let largest = nums[0];
+    let incr = 1;
+    while (incr < nums.length) {
+        let secondLargest = nums[incr];
+        if (secondLargest != largest) {
+            return secondLargest;
+        }
+        incr++
+    }
+}
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
