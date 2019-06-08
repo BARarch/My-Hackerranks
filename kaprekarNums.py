336730df28d3171cb13819628b3fb595b315ddb3
BARarch <anthonyquivers@gmail.com>
2019-06-08 16:27:07 -0400

    Problem Started

---
+++ kaprekarNums.py
@@ -0,0 +1,18 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 190608
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kaprekarNumbers function below.
+def kaprekarNumbers(p, q):
+
+if __name__ == '__main__':
+    p = int(input())
+
+    q = int(input())
+
+    kaprekarNumbers(p, q)
