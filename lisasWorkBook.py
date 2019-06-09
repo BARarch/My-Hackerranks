419959dcaaed8b2b22b7b98be194142dfc3be36a
BARarch <anthonyquivers@gmail.com>
2019-06-08 21:32:35 -0400

    Submitted on 19-06-08 at 21:32:35 Slow

--- lisasWorkBook.py
+++ lisasWorkBook.py
@@ -9,12 +9,32 @@ import sys
 
 # Complete the workbook function below.
 def workbook(n, k, arr):
+    startPage = 1
+    nSpecialNumbers = 0
 
-    def page(k, arr):
-    ## there can only be one special number per page
-        page = 1
-        for nProbs in arr:
-            while nProbs:
+    #print('k = ' + str(k))
+    #print()
+
+    for nProbs in arr:      ## For every chapter
+        reqPages = math.ceil(nProbs / k)
+        firstProbOnPage = 1
+        #print()
+        #print("chapter with " + str(nProbs) + " problems")
+        for page in range(startPage, startPage + reqPages):  ## For every page in chapter
+            #print("Page Number " + str(page) + str(list(range(firstProbOnPage, firstProbOnPage + k))), end=' ')
+    
+            if page in list(range(firstProbOnPage, firstProbOnPage + k)):
+                ## This is a special Number
+                if page <= nProbs:
+                    nSpecialNumbers += 1
+                    #print("Special Number")
+
+
+            firstProbOnPage += k
+
+        startPage += reqPages
+
+    return nSpecialNumbers  
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
