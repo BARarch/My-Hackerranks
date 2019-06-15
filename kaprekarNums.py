193d1f251e28e6338706ecb959030691698a666c
BARarch <anthonyquivers@gmail.com>
2019-06-15 16:17:29 -0400

    Submitted on 19-06-15 at 16:17:26 Warming

--- kaprekarNums.py
+++ kaprekarNums.py
@@ -9,6 +9,29 @@ import sys
 
 # Complete the kaprekarNumbers function below.
 def kaprekarNumbers(p, q):
+    keps = []
+    for n in range(p, q + 1):
+        if n ** 2 < 10:
+            s = '0' + str(n ** 2)
+        else:
+            s = str(n ** 2)
+        #print(s)
+        #print(len(s))
+        l = int(s[:(len(s) // 2)])
+        r = int(s[(len(s) // 2):])
+
+        #print('left:' + str(l))
+        #print('right:' + str(r))
+
+        if ((l + r) == n):
+            keps.append(n)
+
+    if keps:
+        print(' '.join(map(str, keps)))
+    else:
+        print('INVALID RANGE')
+
+        
 
 if __name__ == '__main__':
     p = int(input())
