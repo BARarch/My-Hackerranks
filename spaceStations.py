3962a39f7db42e77e8b0ca421bd12aa8f7c94eea
BARarch <anthonyquivers@gmail.com>
2019-06-15 17:17:06 -0400

    Submitted on 19-06-15 at 17:17:03

--- spaceStations.py
+++ spaceStations.py
@@ -9,6 +9,25 @@ import sys
 
 # Complete the flatlandSpaceStations function below.
 def flatlandSpaceStations(n, c):
+    dists = []
+    prev = None
+    for spaceStation in sorted(c):
+        if prev != None:
+            dists.append(spaceStation - prev)
+
+        else:
+            # The distance from the first space station and the first city
+            pre = spaceStation
+        
+        prev = spaceStation
+
+    #dont for get the last gap
+    if len(dists) == 0:
+        dists = [0]
+
+    post = n - list(sorted(c))[-1] -  1
+
+    return max([pre, max(dists) // 2, post])
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
