201846c03f6d59a282df2dda1d3ce63e112d1177
BARarch <anthonyquivers@gmail.com>
2019-06-29 16:15:30 -0400

    Submitted on 19-06-29 at 16:15:27 YEA

--- happyLadyBugs.py
+++ happyLadyBugs.py
@@ -9,7 +9,45 @@ import re
 import sys
 
 # Complete the happyLadybugs function below.
+
+
 def happyLadybugs(b):
+    print(b)
+
+    ## Sort and Make sure there is no lonely ladybug
+    if "_" in b:
+        state = "YES"
+        prev = 'a'
+        for color in sorted(b):
+            if color == '_':
+                return state
+            if color != prev:
+                ## Ladybug must be happy by the time we switch colors
+                if state == "NO":
+                    return state
+                else:
+                    state = "NO"
+            else:
+                state = "YES"
+            prev = color
+
+    ## No "_" No Sort
+    else:
+        state = "YES"
+        prev = 'a'
+        for color in b:
+            if color != prev:
+                ## Ladybug must be happy by the time we switch colors
+                if state == "NO":
+                    return state
+                else:
+                    state = "NO"
+            else:
+                state = "YES"
+            prev = color
+
+        return state
+    return 'NO'
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
