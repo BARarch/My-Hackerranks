b1a9b5262c9a87cb9a4400ebd54a6c164f84c75f
BARarch <anthonyquivers@gmail.com>
2019-06-29 16:51:43 -0400

    Problem Started

---
+++ singlyLinkListAtTail.py
@@ -0,0 +1,69 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 190629
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    if head == None:
+        #head = SinglyLinkedList()
+        head = SinglyLinkedListNode(data)
+    
+    else:
+        insertHelper(head, data)
+    return head
+    
+def insertHelper(head, data):
+    #print_singly_linked_list(head, ' ', fptr)
+    if head.next == None:
+        head.next = SinglyLinkedListNode(data)
+    else:
+        insertHelper(head.next, data)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
+
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+
+    fptr.close()
