ce45f349928cd83cdc51c4762c0390cffbb4d0f7
BARarch <anthonyquivers@gmail.com>
2019-07-16 21:33:43 -0400

    preorder swap in order print

--- swapNodes.py
+++ swapNodes.py
@@ -7,26 +7,52 @@ import sys
 #
 # Complete the swapNodes function below.
 #
-def left_child(node):
-    return node[0]
 
-def right_child(node):
-    return node[1]
-
-def node_at(indexes, n):
-    return indexes[n - 1]
 
 def swapNodes(indexes, queries):
     #print(indexes)
-    in_order_indexes(indexes, 1)
-    return [[1]]
+    #print(queries)
+    #n_order_indexes(indexes, 1)
+    def left_child(node):
+        return node[0]
 
-def in_order_indexes(indexes, n):
-    if left_child(node_at(indexes, n)) != -1:
-        in_order_indexes(indexes, left_child(node_at(indexes, n)))    
-    print(n, end=" ")
-    if right_child(node_at(indexes, n)) != -1:
-        in_order_indexes(indexes, right_child(node_at(indexes, n)))
+    def right_child(node):
+        return node[1]
+
+    def node_at(indexes, n):
+        return indexes[n - 1]
+
+    def swap(indexes, n):
+        indexes[n - 1] = list(reversed(indexes[n - 1]))
+
+    def in_order_indexes(indexes, n, res):
+        if left_child(node_at(indexes, n)) != -1:
+            in_order_indexes(indexes, left_child(node_at(indexes, n)), res)    
+        res.append(n)
+        if right_child(node_at(indexes, n)) != -1:
+            in_order_indexes(indexes, right_child(node_at(indexes, n)), res)
+
+    def pre_order_swaps(indexes, n, d, k, res):
+        if (d % k == 0):
+            # Perform Swap
+            swap(indexes, n)
+        if left_child(node_at(indexes, n)) != -1:
+            pre_order_swaps(indexes, left_child(node_at(indexes, n)), d + 1, k, res)      
+        res.append(n)
+        if right_child(node_at(indexes, n)) != -1:
+            pre_order_swaps(indexes, right_child(node_at(indexes, n)), d + 1, k, res)
+
+    outt = []
+    for q in queries:
+        resIn = []
+        pre_order_swaps(indexes, 1, 1, q, resIn)
+        #res = []
+        #in_order_indexes(indexes, 1, res)
+        outt.append(resIn)
+        
+    return outt
+
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
