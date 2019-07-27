be1ccfc2c4573bd0a2563fbf07f0fcd0b87bb5a2
BARarch <anthonyquivers@gmail.com>
2019-07-27 15:40:49 -0400

    Problem Started

---
+++ luckTee.py
@@ -0,0 +1,10 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 190727
+
+def solution(l):
+    return 1
+
+if __name__ == '__main__':
+    n = int(input())
+    codes = map(int, input().split(' '))
+    print(solution(codes))
