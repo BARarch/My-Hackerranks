bb1b6f021936119b0d3e3de57be7e73213de1734
BARarch <anthonyquivers@gmail.com>
2019-08-03 19:41:03 -0400

    worked on first go

---
+++ variableVectorArrays.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, q;
+    scanf("%d %d", &n, &q);
+    vector<int> a[n];           // The golden line, an array of vectors of ints
+
+    int k;
+    for(int i = 0; i < n; i++) {
+        cin >> k;
+        int num;
+        for(int j = 0; j < k; j++) {
+            cin >> num;
+            a[i].push_back(num);
+        }
+    }
+
+    int i;
+    int j;
+    for(int k = 0; k < q; k++) {
+        scanf("%d %d", &i, &j);
+        cout << a[i][j] << "\n";
+    }
+
+    return 0;
+}
