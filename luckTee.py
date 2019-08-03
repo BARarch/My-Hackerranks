af7f79feb208e745852ccb78b43fb751f5d8d551
BARarch <anthonyquivers@gmail.com>
2019-08-03 18:44:47 -0400

    Submitted on 19-08-03 at 18:44:44

--- luckTee.py
+++ luckTee.py
@@ -1,11 +1,47 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
 #Date Started: 190727
 
+
 def solution(l):
+    inds = {}
+
+    ## n^2 factor indexing
+    for ind in range(len(l)):
+        inds[ind] = []
+        for j in range(ind + 1, len(l)):
+            if l[j] % l[ind] == 0:
+                inds[ind].append(j)
+
+    def print_inds(inds):
+        for ind in inds:
+            print(str(ind) + ": " + ' '.join(map(str, inds[ind])))
+
+    
+    
     print(l)
-    return 1
+    print_inds(inds)
+
+    ## Count up lucky triplet paths
+    tot = 0
+    for ind in inds:        
+        for j in inds[ind]:
+            tot += len(inds[j])
+
+    def print_lucky_triplets():
+        pass
+
+    return tot
 
 if __name__ == '__main__':
+    import os
+    import sys
+
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
     n = int(input())
     codes = list(map(int, input().split(' ')))
-    print(solution(codes))
+    result = solution(codes)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
