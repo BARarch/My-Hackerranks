6aa482c7e28ba69ed3b78f5146270e55166c87dc
BARarch <anthonyquivers@gmail.com>
2019-08-22 16:54:23 -0400

    Submitted on 19-08-22 at 16:54:23

--- vectorSort.cpp
+++ vectorSort.cpp
@@ -9,6 +9,20 @@ using namespace std;
 
 
 int main() {
+    int n;
+    cin >> n;
+    vector<int> v;
+    int num;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        v.push_back(num);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     return 0;
 }
