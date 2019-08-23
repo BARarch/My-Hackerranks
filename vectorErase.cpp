69c8d6fdc907a29e48e085506e728be1e6d45fca
BARarch <anthonyquivers@gmail.com>
2019-08-22 20:01:06 -0400

    Problem Started

--- vectorErase.cpp
+++ vectorErase.cpp
@@ -9,6 +9,24 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    vector<int> v;
+    int num;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        v.push_back(num);
+    }
+
+    int i, j, k;
+    cin >> i >> j >> k;
+    v.erase(v.begin() + i - 1);
+    v.erase(v.begin() + j - 1, v.begin() + k - 1);
+    
+    cout << v.size() << "\n";
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+  
     return 0;
 }
