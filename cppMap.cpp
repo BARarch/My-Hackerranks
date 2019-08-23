c5c46f73aeaab749ab8a7105a6cd07499d5cab93
BARarch <anthonyquivers@gmail.com>
2019-08-22 21:54:25 -0400

    Problem Started

---
+++ cppMap.cpp
@@ -0,0 +1,16 @@
+//Date Started: 190822
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
