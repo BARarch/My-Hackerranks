14e0032ed1e2679f84596e0b7948de52e69e7a6a
BARarch <anthonyquivers@gmail.com>
2019-08-22 20:57:49 -0400

    Submitted on 19-08-22 at 20:57:49

--- lowerBound.cpp
+++ lowerBound.cpp
@@ -9,6 +9,29 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    cin >> n;
+    vector<int> v;
+    int num;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        v.push_back(num);
+    } 
+
+    int m;
+    cin >> m;
+    int q;
+    vector<int>::iterator lo;
+    for (int i = 0; i < m; i++) {
+        cin >> q;
+        lo = lower_bound(v.begin(), v.end(), q);
+        if (*lo == q) {
+            cout << "Yes ";
+        } else {
+            cout << "No ";
+        }
+        cout << lo - v.begin() + 1 << "\n";
+    } 
     return 0;
 }
