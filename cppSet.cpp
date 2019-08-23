048a187b919870e37509bae24039deb38167ed86
BARarch <anthonyquivers@gmail.com>
2019-08-22 21:42:12 -0400

    Submitted on 19-08-22 at 21:42:12

--- cppSet.cpp
+++ cppSet.cpp
@@ -10,6 +10,26 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+    int y, x;
+    set<int> s;
+    for (int i = 0; i < n; i++) {
+        cin >> y >> x;
+        if (y == 1) {
+            s.insert(x);
+        }
+        if (y == 2) {
+            s.erase(x);
+        }
+        if (y == 3) {
+            if (s.find(x) == s.end()) {
+                cout << "No " << "\n";
+            } else {
+                cout << "Yes " << "\n";
+            }
+        }
+    }   
     return 0;
 }
