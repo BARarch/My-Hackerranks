661453b86a19ac8a78e736e00a2c101a24d78ba3
BARarch <anthonyquivers@gmail.com>
2019-08-22 22:43:02 -0400

    Submitted on 19-08-22 at 22:43:02

--- cppMap.cpp
+++ cppMap.cpp
@@ -9,8 +9,39 @@
 #include <algorithm>
 using namespace std;
 
+int get_grade(map<string, int> book, string name){
+    map<string, int>::iterator itr = book.find(name);
+    if (itr == book.end()) {
+        return 0;
+    } else {
+        return itr -> second;
+    }
+}
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    int query, y;
+    string x;
+    map<string, int> grades;
+    for (int i = 0; i < n; i++) {
+        cin >> query >> x;
+        switch (query) {
+            case 1:
+                cin >> y;
+                int tempGrade;
+                tempGrade = get_grade(grades, x);
+                //cout << "TempGrade " << tempGrade << endl;
+                grades.erase(x);
+                grades.insert(make_pair(x, (tempGrade + y)));
+                break;
+            case 2:
+                grades.erase(x);
+                break;
+            case 3:
+                cout << get_grade(grades, x) << endl;
+                break;
+        }
+    }
     return 0;
 }
