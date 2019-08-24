071bf389def9a3a3f5fff798f96cbda61f0cd412
BARarch <anthonyquivers@gmail.com>
2019-08-23 21:49:16 -0400

    Submitted on 19-08-23 at 21:49:16

--- cppString.cpp
+++ cppString.cpp
@@ -6,6 +6,34 @@ using namespace std;
 
 int main() {
 	// Complete the program
+    string a;
+    string b;
+
+    cin >> a >> b;
+
+    int la = a.size();
+    int lb = b.size();
+
+    cout << la << " " << lb << endl;
+    cout << a + b << endl;
+
+    for (int i = 0; i < la; i++) {
+        if (i == 0) {
+            cout << b[0];
+        } else {
+            cout << a[i];
+        }
+    }
+
+    cout << " ";
+
+    for (int i = 0; i < lb; i++) {
+        if (i == 0) {
+            cout << a[0];
+        } else {
+            cout << b[i];
+        }
+    }
   
     return 0;
 }
