a9f7f6f2e60b2fd2de84bdb4e26b472e3bc9ad54
BARarch <anthonyquivers@gmail.com>
2019-08-29 14:32:56 -0400

    Problem Started

---
+++ twoStacks.py
@@ -0,0 +1,39 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 190829
+
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the twoStacks function below.
+#
+def twoStacks(x, a, b):
+    #
+    # Write your code here.
+    #
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    g = int(input())
+
+    for g_itr in range(g):
+        nmx = input().split()
+
+        n = int(nmx[0])
+
+        m = int(nmx[1])
+
+        x = int(nmx[2])
+
+        a = list(map(int, input().rstrip().split()))
+
+        b = list(map(int, input().rstrip().split()))
+
+        result = twoStacks(x, a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
