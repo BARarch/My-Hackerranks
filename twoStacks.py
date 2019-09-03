e656fa9858b020b494c09781cbfb50599bc75c42
BARarch <anthonyquivers@gmail.com>
2019-09-02 22:11:46 -0400

    Submitted on 19-09-02 at 22:11:46 Woo

--- twoStacks.py
+++ twoStacks.py
@@ -9,56 +9,12 @@ import sys
 #
 # Complete the twoStacks function below.
 #
-def twoStacks_slow(x, a, b):
-    #
-    # Write your code here.
-    #
-    # binary search tree recusion, perfect test for backtracking
-    # pick the lowest number first
-    # i: position in stack a
-    # j: position in stack b
-    ## Select A
-    from collections import deque
-
-    moves = deque()
-    maxD = 0
-    moves.append((x, 0, 0, 0))
-
-    def x_(move):
-        return move[0]
-
-    def d_(move):
-        return move[1]
-
-    def i_(move):
-        return move[2]
-
-    def j_(move):
-        return move[3]
-
-    pops = 0
-
-    while moves:
-        move = moves.popleft()
-        pops += 1
-        if d_(move) > maxD:
-            maxD = d_(move)
-        if i_(move) < len(a) and a[i_(move)] <= x_(move):
-            # Add this move from this state: a draw from stack A
-            moves.append((x_(move) - a[i_(move)], d_(move) + 1, i_(move) + 1, j_(move)))
-        if j_(move) < len(b) and b[j_(move)] <= x_(move):
-            # Add this move from this state: a draw from stack B
-            moves.append((x_(move) - b[j_(move)], d_(move) + 1, i_(move), j_(move) + 1))
-
-    #print("x = " + str(x))
-    #print("Completed in " + str(pops) + "pops")
-    return maxD
 
 def twoStacks(x, a, b):
     sum0 = 0
     i = 1
     x1 = [0, ]
-    while sum0 + a[i - 1] < x:
+    while sum0 + a[i - 1] <= x:
         sum0 += a[i - 1]
         x1.append(sum0)
         i += 1
@@ -68,7 +24,7 @@ def twoStacks(x, a, b):
     sum0 = 0
     i = 1
     x2 = [0, ]
-    while sum0 + b[i - 1] < x:
+    while sum0 + b[i - 1] <= x:
         sum0 += b[i - 1]
         x2.append(sum0)
         i += 1
@@ -80,13 +36,8 @@ def twoStacks(x, a, b):
     dmax = i
     j = 0
 
-    #print("x1 start: {} \tx2 start: {}".format(len(x1), len(x2)))
-    print("x: " + str(x))
-    print("x1: " + ' '.join(map(str, x1)))
-    print("x2: " + ' '.join(map(str ,x2)))
-
     while i >= 0:
-        while x1[i] + x2[j] < x:
+        while x1[i] + x2[j] <= x:
             if i + j > dmax:
                 dmax = i + j
             j += 1
@@ -96,85 +47,8 @@ def twoStacks(x, a, b):
             break
         i -= 1
 
-    print(dmax)
     return dmax
 
-
-
-
-
-def twoStacksSloppy(x, a, b):
-    ## This is two stacks
-    ## with two actual stacks
-    from collections import deque
-
-    x1 = deque()
-    # rolling sum for stack a only, this one is popped from the right side
-    x2 = deque()
-    # rolling sum for stack b only, this one is popped from the left side
-
-
-    sum0 = 0
-    x1.append(sum0)
-    for num in a:
-        if sum0 + num <= x:
-            sum0 += num
-            x1.append(sum0)
-        else:
-            break
-
-
-
-    sum0 = 0
-    #x2.append(sum0)
-    for num in b:
-        if sum0 + num <= x:
-            sum0 += num
-            x2.append(sum0)
-        else:
-            break
-
-    dmax = len(x1) - 1
-    d = dmax
-
-    i = x1.pop()
-    j = 0
-    nRuns = 0
-    nRight = 0
-    nUp = 0
-
-    print("x1 start: {} \tx2 start: {}".format(len(x1), len(x2)))
-
-    while x1 and x2:
-        #print("dmax pre is: " + str(dmax))
-        nRuns += 1
-        j = x2.popleft()
-        if i + j <= x:
-            nRight += 1
-            d += 1
-            if d > dmax:
-                dmax = d
-        else:
-            nUp += 1
-            i = x1.pop()
-            x2.appendleft(j)
-            d -= 1
-        #print("dmax post is: " + str(dmax))
-
-    if (not x1) and (i + j <= x):
-        d += 1
-        if d > dmax:
-            dmax = d
-
-    #print("x1 end: {}   \tx2 end: {}".format(len(x1), len(x2)))
-    #print("Runs: {} \t Right: {}\t Up: {}".format(nRuns, nRight, nUp))
-
-    return dmax
-        
-
-
-
-        
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -194,7 +68,7 @@ if __name__ == '__main__':
 
         b = list(map(int, input().rstrip().split()))
 
-        result = twoStacks_slow(x, a, b)
+        result = twoStacks(x, a, b)
 
         fptr.write(str(result) + '\n')
 
