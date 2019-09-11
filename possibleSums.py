fd766f2c17d32d9c78f9e84d1870c91be4223565
BARarch <anthonyquivers@gmail.com>
2019-09-11 17:12:15 -0400

    Problem Started

---
+++ possibleSums.py
@@ -0,0 +1,16 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 190911
+
+def possibleSums(coins, quantity):
+	pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+
+
+
+    fptr.write("Hello Worlds")
+    fptr.write('\n')
+
+    fptr.close()
