36cc0ff9e922ace8c9a9409306a74ecf29a6309d
BARarch <anthonyquivers@gmail.com>
2019-09-11 14:58:31 -0400

    Problem Started

---
+++ reverseKGroups.py
@@ -0,0 +1,21 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 190911
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        result = absolutePermutation(n, k)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
