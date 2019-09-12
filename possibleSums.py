555262257593c990a8c6f47845b785e208c6da8c
BARarch <anthonyquivers@gmail.com>
2019-09-11 20:58:09 -0400

    Submitted on 19-09-11 at 20:58:05 beautifulWork

--- possibleSums.py
+++ possibleSums.py
@@ -1,16 +1,39 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
 #Date Started: 190911
+import os
+from cs_utils import *
 
 def possibleSums(coins, quantity):
-	pass
+    sums = {0: '',}
+    for coin, count in zip(coins, quantity):
+        #print(len(sums))
+        new = {}
+        for n in range(1, count + 1):
+            for summ in sums:
+                if (summ + (n * coin)) not in sums:
+                    #print("summ: {}   n * coin: {}   n: {}".format(summ, n * coin, n))
+                    new[summ + (n * coin)] = ''
+        for summ in new:
+            sums[summ] = ''
+
+            #print(sums)
+
+        
+
+    return len(sums) - 1
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
+    coins = list_string_to_list(input())
+    quantity = list_string_to_list(input())
+
+    print("coins: {}".format(coins))
+    print("quantity: {}".format(quantity))
 
 
-
-    fptr.write("Hello Worlds")
+    fptr.write(str(possibleSums(coins, quantity)))
     fptr.write('\n')
 
     fptr.close()
