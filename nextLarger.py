8c53813d1112e6f92c1c6af7ecc54f5ce932c6cd
BARarch <anthonyquivers@gmail.com>
2019-09-18 19:27:40 -0400

    Submitted on 19-09-18 at 19:27:40

--- nextLarger.py
+++ nextLarger.py
@@ -1,17 +1,41 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
 #Date Started: 190918
 
+def nextLarger(a):
+    from collections import deque
+
+    largerQ = deque()
+    solnQ = deque()
+
+    def head(q):
+        if len(q):
+            return q[len(q) - 1]
+        else:
+            return -1
+
+    for num in reversed(a):
+        while head(largerQ) != -1 and head(largerQ) <= num:
+            largerQ.pop()
+
+        solnQ.appendleft(head(largerQ))
+        largerQ.append(num)
+
+    return list(solnQ)
+
+        
+
+
 if __name__ == '__main__':
+    import os
+    from cs_utils import *
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    string = input()
-    swaps = list_string_to_list(input())
+    a = list_string_to_list(input())
 
-    print("string: {}".format(string))
-    print("swaps: {}".format(swaps))
+    print("input: {}".format(a))
 
 
-    fptr.write(swapLexOrder(string, swaps))
+    fptr.write(list_to_string(nextLarger(a)))
     fptr.write('\n')
 
     fptr.close()
