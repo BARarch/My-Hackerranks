f0188eb730f26985d87c55ddfcdb429d0b16fa4b
BARarch <anthonyquivers@gmail.com>
2019-09-20 21:10:54 -0400

    Submitted on 19-09-20 at 21:10:54

--- climbingStairs.py
+++ climbingStairs.py
@@ -6,10 +6,48 @@ def climbingStairs(n):
     q = deque()
     q.append(n)
     summ = 0
+    sums = {1: 1,
+            2: 2}
     
     while q:
-        if n == 1:
+        s = q.pop()
+        if s == 1:
             summ += 1
-        if n == 2:
+        elif s == 2:
             summ += 2
         else:
+            if s in sums:
+                if type(sums[s]) is int:
+                    summ += sums[s]
+                    #print('int hash sum added')
+                else:
+                    a = sums[sums[s][0]] + sums[sums[s][1]]
+                    sums[s] = a
+                    summ += a
+                    #print('tuple sum added {}'.format(a))
+            else:
+                sums[s] = (s - 1, s - 2)
+                q.append(s - 1)
+                q.append(s - 2)
+
+    #print(sums)
+    return summ
+
+
+
+
+
+if __name__ == '__main__':
+    import os
+    from cs_utils import *
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    stairs = int(input())
+
+    print("input: {}".format(stairs))
+
+
+    fptr.write(str(climbingStairs(stairs)))
+    fptr.write('\n')
+
+    fptr.close()
