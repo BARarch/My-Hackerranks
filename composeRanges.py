90ce1a4a005a0a3bef887ce8d9dca2dcf5424d20
BARarch <anthonyquivers@gmail.com>
2019-09-22 16:08:09 -0400

    Submitted on 19-09-22 at 16:08:09

--- composeRanges.py
+++ composeRanges.py
@@ -1,6 +1,30 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
 #Date Started: 190922
 
+def composeRanges(nums):
+    if len(nums) == 0:
+        return []
+    prev = nums[0]
+    rangeA = nums[0]
+    ranges = []
+    for n in nums[1:]:
+        if n == prev + 1:
+            prev = n
+        else:
+            if rangeA == prev:
+                ranges.append(str(prev))
+            else:
+                ranges.append('->'.join((str(rangeA), str(prev))))
+            rangeA = n
+            prev = n
+
+    if rangeA == prev:
+        ranges.append(str(prev))
+    else:
+        ranges.append('->'.join((str(rangeA), str(prev))))
+
+    return ranges
+
 if __name__ == '__main__':
     import os
     from cs_utils import *
@@ -11,7 +35,7 @@ if __name__ == '__main__':
     print("input: {}".format(nums))
 
 
-    fptr.write(str(houseRobber(nums)))
+    fptr.write(str(composeRanges(nums)))
     fptr.write('\n')
 
     fptr.close()
