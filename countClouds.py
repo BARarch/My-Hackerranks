a116d1eb9a4f119e4943808904d1963948c97c69
BARarch <anthonyquivers@gmail.com>
2019-09-27 17:10:22 -0400

    Problem Started

--- countClouds.py
+++ countClouds.py
@@ -1,19 +1,5 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
-#Date Started: 190919
+#Date Started: 190927
+
 def countClouds(skyMap):
-    return 4
-
-if __name__ == '__main__':
-    import os
-    from cs_utils import *
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    skyMap = list_string_to_list(input())
-
-    print("input: {}".format(skyMap))
-
-
-    fptr.write(str(countClouds(skyMap)))
-    fptr.write('\n')
-
-    fptr.close()
+	pass
