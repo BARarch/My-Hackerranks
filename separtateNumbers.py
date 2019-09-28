43a9a4021c8b80e7a213dce0ba1472acc0325508
BARarch <anthonyquivers@gmail.com>
2019-09-28 19:30:57 -0400

    Problem Started

---
+++ separtateNumbers.py
@@ -0,0 +1,21 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 190928
+
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the separateNumbers function below.
+def separateNumbers(s):
+    
+
+if __name__ == '__main__':
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        separateNumbers(s)
