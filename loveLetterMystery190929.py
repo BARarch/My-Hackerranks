4d46d855ab77ed26c8c769b8897f3c1236882b1a
BARarch <anthonyquivers@gmail.com>
2019-09-29 15:54:39 -0400

    It looks like cleavland is going to win

---
+++ loveLetterMystery190929.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the theLoveLetterMystery function below.
+def theLoveLetterMystery(s):
+    flips = 0
+    for i in range(len(s) // 2):
+        flips += abs(ord(s[i]) - ord(s[-(i + 1)]))
+    return flips
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
