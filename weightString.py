fd895406389e33ea43a9b080921a19a6c1e3b491
BARarch <anthonyquivers@gmail.com>
2019-10-02 12:37:23 -0400

    Submitted on 19-10-02 at 12:37:23 warmingUp

--- weightString.py
+++ weightString.py
@@ -11,6 +11,32 @@ import sys
 
 # Complete the weightedUniformStrings function below.
 def weightedUniformStrings(s, queries):
+    from itertools import count
+    import string
+    weightIter = count(1)
+    weights = {x: next(weightIter) for x in string.ascii_lowercase}
+
+    substringWeights = {}
+    prev = '?'
+    substringSum = 0
+    for c in s:
+        if c == prev:
+            substringSum += weights[c]        
+        else:
+            substringSum = weights[c]
+
+        if substringSum not in substringWeights:
+                substringWeights[substringSum] = ''
+        prev = c
+
+    res = []
+    for q in queries:
+        if q in substringWeights:
+            res.append("Yes")
+        else:
+            res.append("No")
+    
+    return res
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
