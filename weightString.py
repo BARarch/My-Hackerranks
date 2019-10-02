9ab8c06db06a73313168da485224eededf803511
BARarch <anthonyquivers@gmail.com>
2019-10-02 12:21:15 -0400

    Problem Started

---
+++ weightString.py
@@ -0,0 +1,33 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 191002
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the weightedUniformStrings function below.
+def weightedUniformStrings(s, queries):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = int(input())
+        queries.append(queries_item)
+
+    result = weightedUniformStrings(s, queries)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
