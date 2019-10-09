cebc85c960bb298be79ab442f6f8fe27a99c05d5
BARarch <anthonyquivers@gmail.com>
2019-10-08 20:39:32 -0400

    Submitted on 19-10-08 at 20:39:32

--- fillingBlocks.py
+++ fillingBlocks.py
@@ -2,4 +2,44 @@
 #Date Started: 191008
 
 def fillingBlocks(n):
-    pass
+    blocksHash = {0: 1}
+
+    def fillingBlocksHelp(n):
+        if n in blocksHash:
+            return blocksHash[n]
+
+        fits = 0
+
+        for i in range(n):
+            j = n - i
+            if j == 1:
+                fits += fillingBlocksHelp(i)
+            elif j == 2:
+                fits += 4 * fillingBlocksHelp(i)
+            elif j % 2: # j is odd, there are 2 indivisibles
+                fits += 2 * fillingBlocksHelp(i)
+            else:  # j is even, there are 3 indivisibles
+                fits += 3 * fillingBlocksHelp(i)
+
+        blocksHash[n] = fits    
+        return fits
+
+    return fillingBlocksHelp(n)
+
+    
+    
+
+    
+
+if __name__ == '__main__':
+    import os
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    print("n: {}".format(n))
+
+    fptr.write(str(fillingBlocks(n)))
+    fptr.write('\n')
+
+    fptr.close()
