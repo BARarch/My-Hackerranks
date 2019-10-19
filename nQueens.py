e99f9312f68aeebb1f92272fd0315d33eef49ff6
BARarch <anthonyquivers@gmail.com>
2019-10-19 15:41:01 -0400

    Submitted on 19-10-19 at 15:41:01 Genius

--- nQueens.py
+++ nQueens.py
@@ -2,7 +2,26 @@
 #Date Started: 191019
 
 def nQueens(n):
-    return [[5,5], [5,5]]
+    # What is a viable solution for each colunm and colunm value:
+    #   Positions not in the right, up, and down sets
+    # What is the rejection criteria, how do we implement it?
+    #   Pruning, modify the right, up, and down, sets at each level
+    #   and difference them from the set of all posible positions
+    def nQueensHelp(n, viable, right, up, down):
+        # That prune set has three components right, up, and down
+        if len(viable) == n:
+            return [viable, ]
+        solns = []
+        viables = {x for x in range(1, n + 1)} - (right | up | down) 
+        for x in viables:
+            solns += nQueensHelp(   n, 
+                                    viable + [x, ], 
+                                    right | {x}, 
+                                    {y - 1 for y in up} | {x - 1}, 
+                                    {y + 1 for y in down} | {x + 1}) 
+        return solns
+
+    return list(sorted(nQueensHelp(n, [], {0}, {0}, {n + 1})))
 
 if __name__ == '__main__':
     import os
