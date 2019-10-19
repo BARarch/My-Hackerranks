f298103d599985417461f8bcee3e7e565ee5d104
BARarch <anthonyquivers@gmail.com>
2019-10-19 14:30:15 -0400

    Maximal Square and NQueens Setup

--- maximalSquare.py
+++ maximalSquare.py
@@ -2,3 +2,18 @@
 #Date Started: 191019
 
 def maximalSquare(matrix):
+    print(len(matrix))
+    return 1
+
+import cs_utils
+import os
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    mat = cs_utils.list_string_to_list(input())
+
+    fptr.write(str(maximalSquare(mat)))
+    fptr.write('\n')
+
+    fptr.close()
