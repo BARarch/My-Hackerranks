b62c704e2fa3949e3335cf45e20765305f4183a4
BARarch <anthonyquivers@gmail.com>
2019-10-19 14:22:58 -0400

    Problem Started

--- nQueens.py
+++ nQueens.py
@@ -2,3 +2,17 @@
 #Date Started: 191019
 
 def nQueens(n):
+    return [[5,5], [5,5]]
+
+if __name__ == '__main__':
+    import os
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    print("n: {}".format(n))
+
+    fptr.write(str(nQueens(n)))
+    fptr.write('\n')
+
+    fptr.close()
