e362e6301e959b32f9ea11d3b5c6e4e83dcb8aeb
BARarch <anthonyquivers@gmail.com>
2019-10-30 18:56:25 -0400

    Work through little errors

---
+++ javaEndOfFile.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = 0;
+        while (scan.hasNext()) {
+            i++;
+            System.out.printf("%d %s\n",i, scan.nextLine());
+        }
+        
+    }
+}
+
