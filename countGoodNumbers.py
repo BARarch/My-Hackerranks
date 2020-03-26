123cca6430366967bfc326bd500dd1b5125bce2f
BARarch <anthonyquivers@gmail.com>
2020-03-26 22:36:23 +0100

    Submitted on 20-03-26 at 22:36:21

--- countGoodNumbers.py
+++ countGoodNumbers.py
@@ -2,8 +2,24 @@
 #Date Started: 200326
 
 def countNumbers(arr):
+    res = []
+    
+    def countNumbersPair(pair):
+        good = 0
+        for i in map(str, range(pair[0], pair[1] + 1)):
+            #print (i, end=" ")
+            if len(i) == len(set(i)):
+                good += 1
+                #print("Good", end="")
+
+        print(good)
+        return good
+
+    return list(map(countNumbersPair, arr))
 
 if __name__ == '__main__':
+    import os
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
     arr_rows = int(input().strip())
     arr_columns = int(input().strip())
 
@@ -12,4 +28,9 @@ if __name__ == '__main__':
     for _ in range(arr_rows):
         arr.append(list(map(int, input().rstrip().split())))
 
-    countNumbers(arr)
+    result = countNumbers(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
