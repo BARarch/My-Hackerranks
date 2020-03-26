08042a94964579794e80a68332f62ab467c1c0d8
BARarch <anthonyquivers@gmail.com>
2020-03-26 18:09:44 +0100

    Problem Started

---
+++ floorNumbering.py
@@ -0,0 +1,30 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 200326
+
+# Complete the getLuckyFloorNumber function below.
+def getLuckyFloorNumber(n):
+    def luckyFloorNumber():
+        floor = 0
+        while True:
+            floor += 1
+            if '4' in str(floor) or '13' in str(floor):
+                continue
+            yield floor
+
+    floors = luckyFloorNumber()
+    for _ in range(n):
+        luckyFloor = next(floors)
+
+    return luckyFloor
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    res = getLuckyFloorNumber(n)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
