7b1e0d8521ebf6c6cee85d00fb882ec6d0ff8b1f
BARarch <anthonyquivers@gmail.com>
2020-03-27 14:25:15 +0100

    Submitted on 20-03-27 at 14:25:10

--- differentRightMostBit.py
+++ differentRightMostBit.py
@@ -1,14 +1,17 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
 #Date Started: 200327
 
+def differentRightmostBit(n, m):
+    return list(filter(lambda y: ((n ^ m) // y) % 2, map(lambda x: 2 ** x, range(30))))[0]
+
 if __name__ == '__main__':
     import os
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
+    n = int(input())
+    m = int(input())
 
-
-
-    fptr.write("Hello Worlds")
+    fptr.write(str(differentRightmostBit(n, m)))
     fptr.write('\n')
 
     fptr.close()
--- swapAdjacentBits.py
+++ swapAdjacentBits.py
@@ -2,7 +2,7 @@
 #Date Started: 200327
 
 def swapAdjacentBits(n):
-    print(list(map(lambda x: (n // (2 ** (2 * x)) % 4) , range(16))))
+    #print(list(map(lambda x: (n // (2 ** (2 * x)) % 4) , range(16))))
     return sum(map(lambda b: b[0] * b[1] ,zip(map(lambda a: 2 ** (2 * a), range(16)) , map(lambda y: (y * 2 + ((y * 2) // 4)) % 4 , map(lambda x: (n // (2 ** (2 * x))) % 4 , range(16))))))   
 
 if __name__ == '__main__':
