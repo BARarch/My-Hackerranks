6abcac3c695c7ccdd8b977c38ffb4c8664719813
BARarch <anthonyquivers@gmail.com>
2020-03-28 13:44:14 +0100

    Submitted on 20-03-28 at 13:44:09

--- cs_base.py
+++ cs_base.py
@@ -1,5 +1,8 @@
+
+
 if __name__ == '__main__':
     import os
+    import cs_utils
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
 
--- equalPairOfBits.py
+++ equalPairOfBits.py
@@ -1,14 +1,19 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
 #Date Started: 200328
 
+def equalPairOfBits(n, m):
+    return next(filter(lambda y: ((~(n ^ m)) // y) % 2, map(lambda x: 2 ** x, range(30))))
+    
 if __name__ == '__main__':
     import os
+    import cs_utils
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
+    n = int(input())
+    m = int(input())
 
 
-
-    fptr.write("Hello Worlds")
+    fptr.write(str(equalPairOfBits(n, m)))
     fptr.write('\n')
 
     fptr.close()
