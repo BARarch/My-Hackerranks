6d9302dff71763f3907cccc08c1de6fab25cf667
BARarch <anthonyquivers@gmail.com>
2020-03-28 16:20:45 +0100

    Submitted on 20-03-28 at 16:20:43

--- palendromeOut.py
+++ palendromeOut.py
@@ -1,23 +1,20 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
 #Date Started: 200328
 
-def countRomanXs(n):
-    print(n)
-    n = str(n)
-    xs = 0
-    if n[-1] == '9':
-        xs += 1
+def palandrome3(n):
+    def reverse(s): 
+        str = "" 
+        for i in s: 
+            str = i + str
+        return str
 
-    if len(n) > 1:
-        d = n[-2]
-        if int(d) % 5 == 4:
-            xs += 1
-        else:
-            xs += int(d) % 5 
-    return xs
+    return int(reverse(str(n)) + str(n))
 
-def countInRange(a, b):
-    return sum(map(countRomanXs, range(1, b + 1)))
+
+def sumPalendromes():
+    return sum(map(palandrome3, range(100)))
+    
+
 
 if __name__ == '__main__':
     import os
@@ -26,10 +23,10 @@ if __name__ == '__main__':
 
     #n = int(input())
     a = int(input())
-    b = int(input())
+    #b = int(input())
 
 
-    fptr.write(str(countInRange(a, b)))
+    fptr.write(str(sumPalendromes()))
     fptr.write('\n')
 
     fptr.close()
