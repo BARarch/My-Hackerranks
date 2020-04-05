f8726bd01b63451fe9dc2c0fac5c73f0baf3a2ea
BARarch <anthonyquivers@gmail.com>
2020-04-05 12:31:08 -0400

    Submitted on 20-04-05 at 12:31:08

--- isPower.py
+++ isPower.py
@@ -1,6 +1,20 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
 #Date Started: 200405
 
+def isPower(n):
+    from itertools import count
+    print('Testing {}: '.format(n))
+    if n == 1 or n == 0:
+        return True
+
+    for a in count(2): 
+        if a == n:
+            return False
+        for b in count(2):
+            if a ** b == n:
+                return True
+            if a ** b > n:
+                break
 
 
 if __name__ == '__main__':
@@ -10,10 +24,10 @@ if __name__ == '__main__':
 
     #l = list_string_to_list(input())
     #ll = list_string_to_linked_list(input())
-    #i = int(input())
+    n = int(input())
     #s = input()
 
-    fptr.write(str("Function Call to Return Answer Here"))
+    fptr.write(str(isPower(n)))
     fptr.write('\n')
 
     fptr.close()
