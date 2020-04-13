a9712f9a5f53eeab68d17cc1a8a01f45c1ce7950
BARarch <anthonyquivers@gmail.com>
2020-04-12 22:40:53 -0400

    that was not as comfortable as advertised

---
+++ comfortableNumbers200412.py
@@ -0,0 +1,20 @@
+def comfortableNumbers(l, r):
+    def upper(a):
+        return a + sum(map(int, str(a)))
+
+    def lower(a):
+        return a - sum(map(int, str(a)))
+
+    def test_b(a):
+        def test(b):
+            return a >= lower(b)
+        return test
+
+    H2 = {a: list(filter(test_b(a), range(a + 1, min(r + 1, upper(a) + 1)))) for a in range(l, r + 1)}
+    #print(H2)
+
+    res = 0
+    for o in H2:
+        res += len(H2[o])
+
+    return res
