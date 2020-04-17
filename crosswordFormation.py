6dc9d4b3539dcd349e89090151a62d502402e92c
BARarch <anthonyquivers@gmail.com>
2020-04-17 18:18:13 -0400

    Problem Started

---
+++ crosswordFormation.py
@@ -0,0 +1,20 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 200417
+
+
+
+if __name__ == '__main__':
+    import os
+    from cs_utils import *
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    #l = list_string_to_list(input())
+    #ll = list_string_to_linked_list(input())
+    #i = int(input())
+    #s = input()
+
+    fptr.write(str("Function Call to Return Answer Here"))
+    fptr.write('\n')
+
+    fptr.close()
+
