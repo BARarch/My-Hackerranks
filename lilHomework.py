00d176994ea2e5c5c4a1b2e85f0dadf12390113c
BARarch <anthonyquivers@gmail.com>
2020-07-09 13:18:41 -0400

    Submitted on 20-07-09 at 13:18:33

--- lilHomework.py
+++ lilHomework.py
@@ -6,9 +6,81 @@ import os
 import random
 import re
 import sys
+from qtimer import timeit
+
 
 # Complete the lilysHomework function below.
+@timeit
 def lilysHomework(arr):
+    from collections import deque
+
+    def swapSort_one_way(arr1):
+        A = sorted(arr1)
+
+        H = {val: index for index, val in enumerate(arr1)}
+        swaps = 0
+        for i in range(len(arr1)):
+            if arr1[i] != A[i]:
+                swaps += 1
+                j = H[A[i]]
+                H[arr1[i]] = H[A[i]]
+                arr1[i], arr1[j] = A[i], arr1[i]
+
+        return swaps
+
+    ## create hashmap value -> index
+    def swapSort(arr1):
+        A = deque(sorted(arr1))
+        H = {val: index for index, val in enumerate(arr1)}
+
+        #H1 = {}
+        #for index, val in enumerate(arr1):
+        #    if val in H1:
+        #        H1[val].append(index)
+        #        print("Arr has a reapeat: {}".format(val))
+        #    else:
+        #
+        #        H1[val] = [
+        #            index,
+        #        ]
+        #print(len(H))
+        #print(len(H1))
+
+        start = 0
+        end = len(arr) - 1
+        swaps = 0
+        #print(arr1)
+        while start < end:
+            maxNum = A.pop()
+            minNum = A.popleft()
+            if end != H[maxNum]:
+                #Perform Max Swap
+                a = arr1[end]
+                arr1[end] = maxNum
+                arr1[H[maxNum]] = a
+                H[a] = H[maxNum]
+                H[maxNum] = end
+                swaps += 1
+            if start != H[minNum]:
+                #Perform Min Swap
+                a = arr[start]
+                arr1[start] = minNum
+                arr1[H[minNum]] = a
+                H[a] = H[minNum]
+                H[minNum] = start
+                swaps += 1
+
+            start += 1
+            end -= 1
+            #print(arr1)
+        return swaps
+
+    a = swapSort_one_way(list(arr))
+    print(a)
+    b = swapSort_one_way(list(reversed(arr)))
+    print(b)
+    return min(a, b)
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -22,4 +94,3 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
-
