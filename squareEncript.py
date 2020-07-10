4b5967331fb185005f1fb0c5434a5460ef059cff
BARarch <anthonyquivers@gmail.com>
2020-07-10 16:02:46 -0400

    Submitted on 20-07-10 at 16:02:37 Relentless

--- squareEncript.py
+++ squareEncript.py
@@ -8,9 +8,36 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
-@qtimer.timit
+@qtimer.timeit
 def encryption(s):
+    def square_encryption(s, r, c):
+        def rowIter(chunk):
+            print(chunk)
+            for s in chunk:
+                yield s
+
+        def matrixIterator(s, r, c):
+            for U in zip(*[rowIter(s[x * c:(x + 1) * c]) for x in range(r)]):
+                yield U
+
+        ## Add Spaces to fill rows
+        nAddedSpaces = c - (len(s) % c)
+        s += " " * nAddedSpaces
+
+        ## Additional Row if their are not enough components
+        if r * c < len(s):
+            r += 1
+
+        for i in matrixIterator(s, r, c):
+            yield ''.join(i).strip(' ')
+
+    sr = math.sqrt(len(s))
+    r = math.floor(sr)
+    c = math.ceil(sr)
+    return " ".join(square_encryption(s, r, c))
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
