806a622b7a70cd4d2e8b96faf8a85d0ee8427c10
BARarch <anthonyquivers@gmail.com>
2020-07-11 20:48:59 -0400

    Submitted on 20-07-11 at 20:48:59 NASTY

--- larrysArray.py
+++ larrysArray.py
@@ -8,9 +8,21 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
 def larrysArray(A):
+    # Count inversions
+    inv = 0
+    for i, num in enumerate(A):
+        for otherNum in A[i + 1:]:
+            if num > otherNum:
+                inv += 1
+    if inv % 2 == 0:
+        return "YES"
+    else:
+        return "NO"
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
