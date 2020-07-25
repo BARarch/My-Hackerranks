7ea3df27879a925e1ddbf3905ca9df0b3802a726
BARarch <anthonyquivers@gmail.com>
2020-07-25 18:15:29 -0400

    Problem Started

---
+++ singlePellet.py
@@ -0,0 +1,19 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 200725
+
+import math
+import os
+import random
+import re
+import sys
+import qtimer
+
+# Complete the function below.
+@qtimer.timeit
+def singlePellet(n):
+	return 1
+
+if __name__ == "__main__":
+	n = int(input())
+	print(singlePellet(n))
+
