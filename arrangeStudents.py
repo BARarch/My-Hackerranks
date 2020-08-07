f939b2aab1337a304d077490ba211084690d6aef
BARarch <anthonyquivers@gmail.com>
2020-08-06 21:15:08 -0400

    Submitted on 20-08-06 at 21:15:08

--- arrangeStudents.py
+++ arrangeStudents.py
@@ -8,10 +8,27 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
 def arrangeStudents(a, b):
-    # Write your code here
+    def weaveStudents(a, b):
+        for i, j in zip(sorted(a), sorted(b)):
+            yield i
+            yield j
+
+    last = 0
+    for student in weaveStudents(a, b):
+        if student < last:
+            last = 0
+            for student in weaveStudents(b, a):
+                if student < last:
+                    return "NO"
+                last = student
+            return "YES"
+        last = student
+    return "YES"
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
