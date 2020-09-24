2e15d3ac685f946f74f9cffe6f411142b6052ed1
BARarch <anthonyquivers@gmail.com>
2020-09-24 09:10:15 -0400

    Problem Started

---
+++ minionGame2.py
@@ -0,0 +1,18 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 200924
+
+import math
+import os
+import random
+import re
+import sys
+import qtimer
+
+# Complete the function below.
+@qtimer.timeit
+def minion_game(string):
+    # your code goes here
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
