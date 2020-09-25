d60a946d26788556a00e5418d1101786b774b2e6
BARarch <anthonyquivers@gmail.com>
2020-09-25 09:29:31 -0400

    Problem Started

---
+++ htmlLink.py
@@ -0,0 +1,13 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 200925
+
+import math
+import os
+import random
+import re
+import sys
+import qtimer
+
+# Complete the function below.
+@qtimer.timeit
+
--- minionGame2.py
+++ minionGame2.py
@@ -8,10 +8,33 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
 def minion_game(string):
-    # your code goes here
+    l = len(string)
+    n = 0
+    vowels = "AEIOU"
+    kevinScore = 0
+    stuartScore = 0
+
+    cWords = {}
+    vWords = {}
+
+    for c in string:
+        if c in vowels:
+            kevinScore += l - n
+        else:
+            stuartScore += l - n
+        n += 1
+
+    if kevinScore == stuartScore:
+        print("Draw")
+    elif kevinScore > stuartScore:
+        print('Kevin {}'.format(str(kevinScore)))
+    else:
+        print('Stuart {}'.format(str(stuartScore)))
+
 
 if __name__ == '__main__':
     s = input()
