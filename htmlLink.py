049652a2ab982b0880549f97966cd61abd2e69f0
BARarch <anthonyquivers@gmail.com>
2020-09-29 09:13:39 -0400

    Problem Started

--- htmlLink.py
+++ htmlLink.py
@@ -8,6 +8,22 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
+def htmlLink(text):
+    soup = BeautifulSoup(text, features="lxml")
+    links = soup.select('a')
 
+    for link in links:
+        ref = link['href']
+        content = link.contents[0]
+        print(f'{ref},{content}')
+
+
+if __name__ == "__main__":
+    from bs4 import BeautifulSoup
+    N = int(input())
+    text = "\n".join([input() for _ in range(N)])
+
+    htmlLink(text)
---
+++ tablesAndInters.py
@@ -0,0 +1,21 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 200929
+
+import math
+import os
+import random
+import re
+import sys
+import qtimer
+
+# Complete the function below.
+@qtimer.timeit
+import math
+import os
+import random
+import re
+import sys
+import qtimer
+
+
+# Complete the function below.
