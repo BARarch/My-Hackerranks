f7d874d0ace527230a8df2df4d632509a35a3831
BARarch <anthonyquivers@gmail.com>
2020-09-29 22:28:33 -0400

    Submitted on 20-09-29 at 22:28:33

--- complexNumberClasses.py
+++ complexNumberClasses.py
@@ -8,25 +8,41 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
-import math
+def default():
+    pass
+
 
 class Complex(object):
     def __init__(self, real, imaginary):
         self.real = real
         self.imaginary = imaginary
-        
+
     def __add__(self, no):
-        
-        
+        return Complex(self.real + no.real, self.imaginary + no.imaginary)
+
     def __sub__(self, no):
-        
+        return Complex(self.real - no.real, self.imaginary - no.imaginary)
+
+    def conj(self):
+        return Complex(self.real, -self.imaginary)
+
     def __mul__(self, no):
+        a = self.real
+        b = self.imaginary
+        aN = no.real
+        bN = no.imaginary
+        return Complex((a * aN) - (b * bN), (a * bN) + b * aN)
 
     def __truediv__(self, no):
+        quo = (no * no.conj()).real
+        prod = self * no.conj()
+        return Complex(prod.real / quo, prod.imaginary / quo)
 
     def mod(self):
+        return Complex(math.sqrt(self.real**2 + self.imaginary**2), 0)
 
     def __str__(self):
         if self.imaginary == 0:
@@ -42,9 +58,10 @@ class Complex(object):
             result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
         return result
 
+
 if __name__ == '__main__':
     c = map(float, input().split())
     d = map(float, input().split())
     x = Complex(*c)
     y = Complex(*d)
-    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
+    print(*map(str, [x + y, x - y, x * y, x / y, x.mod(), y.mod()]), sep='\n')
