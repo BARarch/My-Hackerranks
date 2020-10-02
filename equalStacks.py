55143f2ec25802807b9ec8e202618d7a1fe7af25
BARarch <anthonyquivers@gmail.com>
2020-10-02 09:01:30 -0400

    Problem Started

---
+++ equalStacks.py
@@ -0,0 +1,37 @@
+#!/Users/anthonyquivers/anaconda3/bin/python
+#Date Started: 201002
+
+import math
+import os
+import random
+import re
+import sys
+import qtimer
+
+# Complete the function below.
+@qtimer.timeit
+def equalStacks(h1, h2, h3):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n1 = int(first_multiple_input[0])
+
+    n2 = int(first_multiple_input[1])
+
+    n3 = int(first_multiple_input[2])
+
+    h1 = list(map(int, input().rstrip().split()))
+
+    h2 = list(map(int, input().rstrip().split()))
+
+    h3 = list(map(int, input().rstrip().split()))
+
+    result = equalStacks(h1, h2, h3)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
