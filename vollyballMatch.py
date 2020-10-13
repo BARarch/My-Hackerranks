07255258b19b512cd5a49bd09df7a008f985b221
BARarch <anthonyquivers@gmail.com>
2020-10-12 23:12:41 -0400

    Submitted on 20-10-12 at 23:12:35

--- vollyballMatch.py
+++ vollyballMatch.py
@@ -8,7 +8,39 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
-def vollyballOutcomes
-	pass
+def vollyballOutcomes(a, b):
+    if max(a, b) == 25:
+        if a == 25:
+            losingPoints = b
+        elif b == 25:
+            losingPoints = a
+        if losingPoints < 24:
+            return binom(25 + losingPoints - 1, losingPoints) % ((10**9) + 7)
+        else:
+            return 0
+    elif max(a, b) > 25:
+        if abs(a - b) != 2:
+            return 0
+        else:  # TieBreak
+            if a > b:
+                losingPoints = b
+            else:
+                losingPoints = a
+            tieWays = binom(48, 24)
+            return (tieWays * 2**(losingPoints - 24)) % ((10**9) + 7)
+    else:
+        return 0
+
+
+def binom(n, k):
+    return math.factorial(n) // math.factorial(k) // math.factorial(n - k)
+
+
+if __name__ == "__main__":
+    A = int(input())
+    B = int(input())
+
+    print(vollyballOutcomes(A, B))
