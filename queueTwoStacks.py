0d1de2c933bb10ab57e9213c9f1b4bee82901907
BARarch <anthonyquivers@gmail.com>
2020-10-14 10:17:34 -0400

    Submitted on 20-10-14 at 10:17:29

--- queueTwoStacks.py
+++ queueTwoStacks.py
@@ -9,5 +9,89 @@ import sys
 import qtimer
 
 # Complete the function below.
-@qtimer.timeit
 
+push = []
+pop = []
+
+
+def refresh(a, b):
+    while a:
+        b.append(a.pop())
+
+
+def enqueue(num):
+    push.append(num)
+
+
+def front():
+    if pop:
+        return pop[-1]
+    else:
+        refresh(push, pop)
+        return pop[-1]
+
+
+def dequeue():
+    if pop:
+        pop.pop()
+    else:
+        refresh(push, pop)
+        pop.pop()
+
+
+@qtimer.timeit
+def aS(N):
+    for _ in range(N):
+        x = map(int, input().split(' '))
+        cmd = next(x)
+        if cmd == 1:
+            enqueue(next(x))
+        if cmd == 2:
+            dequeue()
+        if cmd == 3:
+            print(front())
+
+
+s1 = []
+s2 = []
+
+
+def q_append(x):
+    if s2:
+        while s2:
+            s1.append(s2.pop())
+    s1.append(x)
+
+
+def q_pop():
+    if s1:
+        while s1:
+            s2.append(s1.pop())
+    s2.pop()
+
+
+def peek():
+    if s1:
+        while s1:
+            s2.append(s1.pop())
+    return s2[-1]
+
+
+@qtimer.timeit
+def pS(q):
+    for i in range(q):
+        line = re.split(' ', sys.stdin.readline())
+        type = int(line[0])
+        if type == 1:
+            x = line[1]
+            q_append(x)
+        if type == 2:
+            q_pop()
+        if type == 3:
+            print(peek(), end="")
+
+
+if __name__ == "__main__":
+    N = int(input())
+    aS(N)
+    #pS(N)
