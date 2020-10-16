46769c35f7949638097148c0c9e7a14df524665a
BARarch <anthonyquivers@gmail.com>
2020-10-16 10:07:52 -0400

    Submitted on 20-10-16 at 10:07:46

--- runningMedian.py
+++ runningMedian.py
@@ -8,20 +8,51 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
-#!/bin/python3
-
-import os
-import sys
-
-#
-# Complete the runningMedian function below.
-#
 def runningMedian(a):
-    #
-    # Write your code here.
-    #
+    from heapq import heappop, heappush, heapify
+
+    low = []  # This is min Heap Multiply by -1
+    heapify(low)
+
+    def heappush_low(h, n):
+        heappush(low, -1 * n)
+
+    def heappop_low(h):
+        return -heappop(h)
+
+    high = []
+    heapify(high)
+
+    med = 0
+    res = []
+    for num in a:
+        #place
+        if num < med:
+            heappush_low(low, num)
+        else:
+            heappush(high, num)
+
+        #asses/shuffle
+        if len(high) - len(low) == 2:
+            heappush_low(low, heappop(high))
+        elif len(low) - len(high) == 2:
+            heappush(high, heappop_low(low))
+
+        #compute med
+        if len(high) > len(low):
+            med = float(high[0])
+        elif len(high) < len(low):
+            med = float(-low[0])
+        else:
+            med = (high[0] - low[0]) / 2
+
+        res.append(med)
+
+    return map(lambda x: str(x), res)
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
