94769f7d81c62741343f96613017add994f6f507
BARarch <anthonyquivers@gmail.com>
2020-10-19 10:11:02 -0400

    Submitted on 20-10-19 at 10:11:02

--- validString.py
+++ validString.py
@@ -8,9 +8,53 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
 def isValid(s):
+    S = {}
+    V = {}
+
+    for c in s:
+        if c in S:
+            S[c] += 1
+        else:
+            S[c] = 1
+
+    for n in S.values():
+        if n in V:
+            V[n] += 1
+        else:
+            V[n] = 1
+
+    #print(s)
+    #print(S)
+    #print(V)
+
+    if len(V) == 1:
+        return "YES"
+
+    ## remove 1 see what happens
+    singleFreq = None
+    for f in V:
+        if V[f] == 1:
+            if singleFreq is None:
+                singleFreq = f
+            else:
+                return "NO"
+
+    if singleFreq:
+        if (singleFreq - 1) == 0:
+            del V[f]
+        elif (singleFreq - 1) in V:
+            V[singleFreq - 1] += 1
+            del V[singleFreq]
+
+    if len(V) == 1:
+        return "YES"
+
+    return "NO"
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
