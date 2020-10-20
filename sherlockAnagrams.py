37441fb18ec7299ce44ea024f0dc1de0a6b7374d
BARarch <anthonyquivers@gmail.com>
2020-10-20 09:57:55 -0400

    Submitted on 20-10-20 at 09:57:49

--- sherlockAnagrams.py
+++ sherlockAnagrams.py
@@ -8,9 +8,29 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
+def substrings(s):
+    return [s[i:j] for i in range(len(s)) for j in range(i + 1, len(s) + 1)]
+
+
 @qtimer.timeit
 def sherlockAndAnagrams(s):
+    subs = substrings(s)
+    matches = {}
+    for substring in substrings(s):
+        sortedSubstring = ''.join(sorted(substring))
+        if sortedSubstring in matches:
+            matches[sortedSubstring] += 1
+        else:
+            matches[sortedSubstring] = 0
+
+    nAnagrams = 0
+    for nMatches in matches.values():
+        nAnagrams += (((nMatches + 1)**2) - (nMatches + 1)) // 2
+
+    return nAnagrams
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
