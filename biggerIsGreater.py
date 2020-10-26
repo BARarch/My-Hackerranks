21ed603bd7ba53a3ac5cf13831f4aa2e404e23fd
BARarch <anthonyquivers@gmail.com>
2020-10-26 09:08:02 -0400

    Problem Started

--- biggerIsGreater.py
+++ biggerIsGreater.py
@@ -1,30 +1,16 @@
 #!/Users/anthonyquivers/anaconda3/bin/python
-#Date Started: 191007
-
-#!/bin/python3
+#Date Started: 201026
 
 import math
 import os
 import random
 import re
 import sys
+import qtimer
 
+# Complete the function below.
+@qtimer.timeit
 def biggerIsGreater(w):
-    greaterList = []
-    for i, c in enumerate(reversed(w)):
-        if not greaterList:
-            greaterList.append(c)
-        elif c >= greaterList[len(greaterList) - 1]:
-            greaterList.append(c)
-        else:
-            for j, letter in enumerate(greaterList):
-                if letter > c:
-                    # Execute First Swap
-                    res = w[:len(w) - i - 1] + letter
-                    greaterList[j] = c
-                    # Sort the rest
-                    return res + ''.join(sorted(greaterList))
-    return 'no answer'
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
