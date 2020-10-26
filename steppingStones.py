eddf76073567af11daf569c26c8f5c26d1fd0819
BARarch <anthonyquivers@gmail.com>
2020-10-26 09:04:07 -0400

    Submitted on 20-10-26 at 09:04:07

--- steppingStones.py
+++ steppingStones.py
@@ -8,9 +8,14 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
 def solve(n):
+    if math.sqrt(1 + (8 * n)).is_integer():
+        return f'Go On Bob {int((math.sqrt(1 + (8 * n)) - 1) // 2)}'
+    return "Better Luck Next time"
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
