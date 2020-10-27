8de06130d1b82a1bed3c18deea641aeb20ff134f
BARarch <anthonyquivers@gmail.com>
2020-10-27 11:14:04 -0400

    Submitted on 20-10-27 at 11:14:04

--- greedyFlorist.py
+++ greedyFlorist.py
@@ -8,9 +8,31 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
+def sorted_costs(p):
+    for c in sorted(p):
+        yield c
+
+
 @qtimer.timeit
 def getMinimumCost(k, c):
+    l = len(c) // k
+    purchasesMax = len(c) % k
+    cost = 0
+
+    prices = sorted_costs(c)
+    for _ in range(purchasesMax):
+        cost += (l + 1) * next(prices)
+
+    nPurchases = l
+    while nPurchases > 0:
+        for _ in range(k):
+            cost += nPurchases * next(prices)
+        nPurchases -= 1
+
+    return cost
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -28,4 +50,3 @@ if __name__ == '__main__':
     fptr.write(str(minimumCost) + '\n')
 
     fptr.close()
-
