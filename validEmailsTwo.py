af904854d968197713d3c5bfff775b4d1881875d
BARarch <anthonyquivers@gmail.com>
2020-11-02 09:48:41 -0500

    Submitted on 20-11-02 at 09:48:35

--- validEmailsTwo.py
+++ validEmailsTwo.py
@@ -9,12 +9,37 @@ import sys
 import qtimer
 
 # Complete the function below.
-@qtimer.timeit
+
+import string
+
+
 def fun(s):
+    if '@' not in s or '.' not in s:
+        return False
+
+    user_name = s[:s.index('@')]
+    if len(user_name) == 0:
+        return False
+    for c in user_name:
+        if c not in string.digits + string.ascii_lowercase + "_" + "-":
+            return False
+    website_name = s[s.index('@') + 1:s.index('.')]
+    if len(website_name) == 0:
+        return False
+    for c in website_name:
+        if c not in string.digits + string.ascii_lowercase:
+            return False
+
+    extention = s[s.index('.') + 1:]
+    return len(extention) <= 3
+
     # return True if s is a valid email, else return False
 
+
+@qtimer.timeit
 def filter_mail(emails):
     return list(filter(fun, emails))
+
 
 if __name__ == '__main__':
     n = int(input())
