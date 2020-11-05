9addc815b861227a5869d5f508f12a589cd532b7
BARarch <anthonyquivers@gmail.com>
2020-11-05 09:17:40 -0500

    Problem Started

--- passengerForcast.py
+++ passengerForcast.py
@@ -8,6 +8,21 @@ import re
 import sys
 import qtimer
 
+
 # Complete the function below.
 @qtimer.timeit
+def passtraffic():
+    pass
 
+
+from collections import defaultdict
+if __name__ == "__main__":
+    N = int(input())
+    H = defaultdict(list)
+    for _ in range(N):
+        line = input().split(' ')
+        month, traffic = int(line[0].split('_')[-1]), int(line[-1])
+        H[month].append(traffic)
+
+    for m in range(1, 13):
+        print(sum(H[m]) / len(H[m]))
