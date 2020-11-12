c91e39bc125f71a2abd7ee413b7dfeade1451e20
BARarch <anthonyquivers@gmail.com>
2020-11-12 11:12:39 -0500

    Submitted on 20-11-12 at 11:12:32

--- waiterPrimes.py
+++ waiterPrimes.py
@@ -8,12 +8,55 @@ import re
 import sys
 import qtimer
 
+
+def gen_primes():
+    D = {}
+    q = 2
+
+    while True:
+        if q not in D:
+            yield q
+            D[q * q] = [q]
+        else:
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
+
+
 # Complete the function below.
+def output_plates(plates):
+    # Output Bs
+    for row in plates:
+        while row[1]:
+            yield row[-1].pop()
+
+    # Output As
+    while plates[-1][0]:
+        yield plates[-1][0].pop()
+
+
 @qtimer.timeit
 def waiter(number, q):
+    primes = gen_primes()
+    plates = [(number, [])]
+    for i in range(q):
+        A_minus_1 = plates[-1][0]
+        A = []
+        B = []
+        prime = next(primes)
+        while A_minus_1:
+            if (A_minus_1[-1] % prime) == 0:
+                B.append(A_minus_1.pop())
+            else:
+                A.append(A_minus_1.pop())
+        plates.append((A, B))
+    return output_plates(plates)
     #
     # Write your code here.
     #
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
