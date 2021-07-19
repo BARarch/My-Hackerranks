eb8a74282250195933f1630f8682f2acaa1f1000
BARarch <anthonyquivers@gmail.com>
2021-07-19 16:54:30 +0200

    Finnished Itertools

---
+++ crazyBall210719.py
@@ -0,0 +1,29 @@
+#Date Started: 210719 
+
+import math
+import os
+import random
+import re
+import sys
+import qtimer
+
+from itertools import combinations
+
+# Complete the function below.
+@qtimer.timeit
+def crazyball(players, k):
+    return list(map(list, sorted(combinations(sorted(players), k))))
+
+if __name__ == "__main__":
+    import cs_utils
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    for var, exp in cs_utils.dict_statement_reader(sys.stdin):
+        exec(var + " = " + exp)
+
+    
+    result = crazyball(players, k)
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
