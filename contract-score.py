f8018a3af05ec5801ade7e8b9ee783b408f2bf0d
BARarch <anthonyquivers@gmail.com>
2023-04-07 13:53:52 -0400

    :tada:max score algorithm structure

---
+++ contract-score.py
@@ -0,0 +1,47 @@
+from typing import List, Set, Tuple
+
+def max_score(viable: Set[int], positionsToChoose: int) -> Tuple[int, List[int]]:
+    ## Input the 
+    #           number of viable contracts and 
+    #              the number of positions to choose  - as in n choose k
+    #  Then output the
+    #                   maximum score and
+    #               the resulting ranked contract choices
+    return 1, list(viable)
+
+
+if __name__ == "__main__":
+    scores0 = [500, 
+               450, 
+               350, 
+               350, 
+               300, 
+               250, 
+               200, 
+               150, 
+               100, 
+               50, 
+               50]
+    
+    viableContracts0 = [[1,4,6,8,9,10],
+                        [1,2,3,5,7,10],
+                        [1,4,6,9,10],
+                        [1,4,10],
+                        [4,7],
+                        [4,6,10],
+                        [1,2,3,4,6,10],
+                        [4],
+                        [4,8],
+                        [1,2,3,5,7,10],
+                        [4,7]]
+    
+    viable0 = set(list(range(1,11)))
+
+    print(max_score(viable0, 10))
+
+
+
+
+
+
+
