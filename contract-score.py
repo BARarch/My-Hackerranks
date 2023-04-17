265f8bd304f6c428c446a2bc4abb0ad7609a4e42
BARarch <pebblebeachprogramming@gmail.com>
2023-04-17 13:13:19 -0400

    :tada: ready for case 1

--- contract-score.py
+++ contract-score.py
@@ -2,49 +2,6 @@ from typing import List, Set, Tuple
 from functools import reduce
 from operator import mul
 
-def max_score(viable: Tuple[int], positionsToChoose: int, scores, viableContracts ) -> Tuple[int, List[int]]:
-    ## Input the 
-    #           number of viable contracts and 
-    #              the number of positions to choose  - as in n choose k
-    #  Then output the
-    #                   maximum score and
-    #               the resulting ranked contract choices
-    MS = {}
-    S = scores
-    VC = viableContracts
-    def resultKey(viableSet, k):
-        return tuple(viableSet), k
-
-    def max_score_helper(viable, positionsToChoose):
-        ## Base Cases
-        if positionsToChoose == 0:
-            return 0, []
-        if resultKey(viable, positionsToChoose) in MS:
-            return MS[resultKey(viable, positionsToChoose)]
-
-        ## Recurse for Viable Solutions at this position
-        currentRow = len(S) - positionsToChoose
-        scoreForRow, contracts = S[currentRow], viableContracts[currentRow]
-        
-        ## FIRST Try Selectiing Nothing at this Stage
-        maxScore, result = max_score_helper(viable, positionsToChoose - 1) 
-        result.append('')
-
-        ## THEN Try all posible selections at this position
-        for choice in set(viable).intersection(contracts):
-            currentScore, currentresult = max_score_helper(tuple(set(viable).difference({choice})), positionsToChoose - 1)
-            if currentScore + scoreForRow > maxScore:
-                currentresult.append(choice)
-                maxScore, result = currentScore + scoreForRow, currentresult
-                positonWasFilled = True
-        
-        return maxScore, result
-
-    maxScore, result = max_score_helper(viable, positionsToChoose)
-    return maxScore, list(reversed(result))
-
-
-
 def max_score_dp(scores, viable):
 
     STATE_OFFSET = 1
@@ -224,13 +181,8 @@ def max_score_dp(scores, viable):
 
     return res, finalState
 
-   
-
-
-
-
-
 if __name__ == "__main__":
+    ## CASE 0
     scores0 = [500, 
                450, 
                350, 
@@ -254,13 +206,35 @@ if __name__ == "__main__":
                         [4,8],          #7
                         [1,2,3,5,7,10], #2 All MATCHED !!!
                         [4,7]]          #
-    
-    viable0 = tuple(range(1,12))
-
 
     max_score_dp(scores0, viableContracts0)
-    #print(max_score(viable0, 11, scores0, viableContracts0))
 
+    ## CASE 1
+    scores1 = [500, 
+               450, 
+               350, 
+               350, 
+               300, 
+               250, 
+               200, 
+               150, 
+               100, 
+               50, 
+               50]
+    
+    viableContracts0 = [[1,4,6,8,9,10], #8
+                        [1,2,3,5,7,10], #4 
+                        [1,4,6,9,10],   #0
+                        [1,4,10],       #9
+                        [4,7],          #6
+                        [4,6,10],       #5
+                        [1,2,3,4,6,10], #1
+                        [4],            #3
+                        [4,8],          #7
+                        [1,2,3,5,7,10], #2 All MATCHED !!!
+                        [4,7]]          #
+
+    max_score_dp(scores0, viableContracts0)
 
 
 
